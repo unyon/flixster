83035e844670209f6e87acd3323fc7060d4eb3eb
unyon <michalloran@hotmail.com>
2016-01-29 02:43:09 +0000

    add sections controller, pages, simple form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Flixter::Application.routes.draw do
+Flixter::Application.routes.draw do;
   devise_for :users
   root 'static_pages#index'
 
@@ -6,7 +6,9 @@ Flixter::Application.routes.draw do
   resources :courses, only: [:index, :show]
 
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create ]
+    end
   end
   
   # The priority is based upon order of creation: first created -> highest priority.
